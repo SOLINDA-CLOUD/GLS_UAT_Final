63aa2fedce5fb827a5e08755e21c7afc4b0bafb2
Luthfi Achmaddin Nizar <Luthfinizar97@gmail.com>
2022-08-15 14:41:12 +0700

    maintenance update

--- sol_boo/models/maintenance_request.py
+++ sol_boo/models/maintenance_request.py
@@ -1,4 +1,5 @@
 from odoo import _, api, fields, models
+from dateutil import relativedelta
 
 class ActionPlanMaintenance(models.Model):
     _name = 'action.plan.maintenance'
@@ -13,18 +14,52 @@ class ActionPlanMaintenance(models.Model):
     ], string='status',default="open")
     maintenance_id = fields.Many2one('maintenance.request', string='Maintenance')
 
+class ProgresHistoryMaintenance(models.Model):
+    _name = 'progres.history.maintenance'
+    _description = 'Progres History Maintenance'
+    
+    start_date = fields.Datetime('Start Date')
+    end_date = fields.Datetime('End Date')
+    stage_from = fields.Char('Stage From')
+    stage_to = fields.Char('Stage To')
+    duration = fields.Char(compute='_compute_duration', string='Duration')
+    maintenance_id = fields.Many2one('maintenance.request', string='Maintenance')
+    
+    @api.depends('start_date','end_date')
+    def _compute_duration(self):
+        for i in self:
+            if i.start_date and i.end_date:
+                diff = relativedelta.relativedelta(i.end_date, i.start_date)
+                years = diff.years
+                months = months
+                days = diff.days
+                hours = diff.hours
+                minutes = diff.minutes
+                if years > 0:
+                    i.duration = str(years) + " Tahun " + str(months) + " bulan " + str(days) + " Hari" + str(hours) + "jam " + str(minutes) + "menit"
+                elif months > 0:
+                    i.duration = str(months) + "Bulan" + str(days) + "hari " + str(hours) + "jam " + str(minutes) + "menit"
+                elif days > 0:
+                    i.duration = str(days) + "Hari " + str(hours) + "jam " + str(minutes) + "menit"
+                elif hours > 0:
+                    i.duration = str(hours) + "Jam " + str(minutes) + "menit"
+                else:
+                    i.duration =str(minutes) + "Menit " + str(diff.seconds) + "detik"
+            else:
+                i.duration = False
 
 class MaintenanceRequest(models.Model):
     _inherit = 'maintenance.request'
     _description = 'Maintenance Request'
     
+    progres_history_line = fields.One2many('progres.history.maintenance', 'maintenance_id', string='Progress History')
     action_plan_line = fields.One2many('action.plan.maintenance', 'maintenance_id', string='Action Plan')
     shutdown_id = fields.Many2one('shutdown.system', string='Shutdown')
     job_order_id = fields.Many2one('job.order.request', string='Job Order') 
-    
+    location_id = fields.Many2one('stock.location', string='Location')
+
     mr_ids = fields.One2many('stock.picking', 'maintenance_id', string='MR')
     mr_count = fields.Integer(compute='_compute_mr_count', string='MR')
-
 
     @api.depends('mr_ids')
     def _compute_mr_count(self):
