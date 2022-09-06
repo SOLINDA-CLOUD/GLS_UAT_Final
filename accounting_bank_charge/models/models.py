345b38c0389868e490df4a357095c4b6a7ba08b1
hamsky13 <ilham.ius13@gmail.com>
2022-09-06 11:11:53 +0700

    tes 3

--- accounting_bank_charge/models/models.py
+++ accounting_bank_charge/models/models.py
@@ -364,8 +364,8 @@ class AccountPayment(models.Model):
                                 "partner_id": self.partner_id.id or False,
                                 "journal_id": self.journal_id.id,
                                 "account_id": tax['account_id'],
-                                "debit": self.bank_tax_amount if self.payment_type == 'inbound' else 0.0,
-                                "credit": self.bank_tax_amount if self.payment_type == 'outbound' else 0.0,
+                                "debit": self.bank_tax_amount if self.payment_type == 'outbound' else 0.0,
+                                "credit": self.bank_tax_amount if self.payment_type == 'inbound' else 0.0,
                                 "date_maturity": self.date,
                                 "bank_tax_charge_line": True
                         })
@@ -377,8 +377,8 @@ class AccountPayment(models.Model):
                             "partner_id": self.partner_id.id or False,
                             "journal_id": self.journal_id.id,
                             "account_id": self.journal_id.default_account_id.id,
-                            "debit": self.bank_tax_amount if self.payment_type == 'outbound' else 0.0,
-                            "credit": self.bank_tax_amount if self.payment_type == 'inbound' else 0.0,
+                            "debit": self.bank_tax_amount if self.payment_type == 'inbound' else 0.0,
+                            "credit": self.bank_tax_amount if self.payment_type == 'outbound' else 0.0,
                             "date_maturity": self.date,
                             "bank_tax_charge_line": True
                         }),
